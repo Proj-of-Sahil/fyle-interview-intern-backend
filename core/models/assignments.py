6a1f5a54db148b4632051ac9e19778b79d33bf05
sahilkamate03 <45514385+sahilkamate03@users.noreply.github.com>
2024-05-13 16:34:07 +0530

    Fix teacher assignments retrieval bug

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -84,14 +84,26 @@ class Assignment(db.Model):
         db.session.flush()
 
         return assignment
+    
+    @classmethod
+    def mark_grade_by_teacher(cls, _id, grade, auth_principal: AuthPrincipal):
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
+        assertions.assert_valid(assignment.teacher_id == auth_principal.teacher_id, 'This assignment belongs to some other teacher')
+        assignment.grade = grade
+        assignment.state = AssignmentStateEnum.GRADED
+        db.session.flush()
+
+        return assignment
 
     @classmethod
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
 
     @classmethod
-    def get_assignments_by_teacher(cls):
-        return cls.query.all()
+    def get_assignments_by_teacher(cls, teacher_id):
+        return cls.filter(cls.teacher_id == teacher_id).all()
     
     @classmethod
     def mark_grade_by_principal(cls, _id, grade, auth_principal: AuthPrincipal):
